bdc8e41703b5c664fd4c224fea3f144edba913b6
Ankush Rana <75374330+Ankush-WD@users.noreply.github.com>
2021-11-20 21:17:31 +0530

    Created setup.php file

---
+++ setup.php
@@ -0,0 +1,15 @@
+<?php
+class start{
+  private $version;
+  public function __construct(){
+    $this->version = "1.0";
+  }
+
+  public function getVersion(){
+    return $this->version
+  }
+}
+
+
+
+?>
